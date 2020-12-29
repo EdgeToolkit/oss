cdbd370e6aa2519908c2e88a9b70f257abfeb4de
Mingyi Zhang <mingyi.z@outlook.com>
2020-12-29 12:41:56 +0000

    builder deployer OK for gcc8

--- .toolkit/ansible/library/gitlab_runner_unregister.py
+++ .toolkit/ansible/library/gitlab_runner_unregister.py
@@ -1,28 +1,7 @@
 #!/usr/bin/env python3
 
 from ansible.module_utils.basic import AnsibleModule
-<<<<<<< HEAD
-from oss_utils.gitlab_runner import GitlabRunnerManager, HashTable
-=======
-from oss_utils.gitlab_runner import GitlabRunnerManager #noqa
->>>>>>> e475437314df87f08d8d5a19db470aa228b15b4d
-def _unregister(param):
-    from oss_utils.gitlab_runner import GitlabRunner, GitlabRunnerDB, load_config #noqa
-    import gitlab
-    db = HashTable(param['db'])
-    config = load_config(param['config'])
-    
-    gl = gitlab.Gitlab(config['gitlab']['url'], config['gitlab']['private_token'])
-    hostname=param['hostname']
-    result = {'changed': False, 'runner': []}
-    for workbench in config['workbench']:
-        for type in param['type']:
-            runner = GitlabRunner(hostname, type=type, workbench=workbench, gitlab=gl, db=db)
-            r = runner.unregister()
-            if r:
-                result['changed'] = True
-                result['runner'].append({'id': r.id, 'name': r.name})
-    return result
+from oss_utils.gitlab_runner import GitlabRunnerManager
 
 def unregister(param):
     manager = GitlabRunnerManager(param['config'], param['db'])
