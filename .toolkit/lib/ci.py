5e45ba8949ee112669fec4efb20e90df0455a1c1
Mingyi Zhang <mingyi.z@outlook.com>
2021-02-10 20:43:53 +0800

    .

--- .toolkit/lib/ci.py
+++ .toolkit/lib/ci.py
@@ -119,7 +119,7 @@ class GitlabRunner(object):
         assert kind in ['builder', 'tester', 'deployer', 'gitlab-ci.config.generator']
 
         filename = f"{self.conf.dir}/tokens.yml"
-        with FileLock(filename, timeout=60):
+        with FileLock(filename, timeout=6):
             runner = self.runners()[0]
             tags = [kind]
             if workbench:
