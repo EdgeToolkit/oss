9eb2218d68787299377a9fc6ba7920c3396fec2f
Mingyi Zhang <mingyi.z@outlook.com>
2021-02-13 10:28:27 +0800

    .

--- .ansible/command/gitlab-ci.py
+++ .ansible/command/gitlab-ci.py
@@ -7,38 +7,7 @@ from epm.utils import Jinja2, abspath
 _DIR = os.path.dirname(abspath(__file__))
 _LIBD = abspath(f"{_DIR}/../lib")
 sys.path.insert(0, _LIBD)
-from configure import Synthesis
 from ci import GitlabRunner
-
-
-def gitlab_ci_generate(args):
-    """generate gitlab ci config file
-    """
-    target = args.target or ['trigger']
-    filename = 'config.yml'
-    synthesis = Synthesis(filename)
-    context = {'synthesis': synthesis}
-    docker_registry_url = os.getenv('DOCKER_REGISTRY_URL')
-    if docker_registry_url:
-        if not docker_registry_url.endswith('/'):
-            docker_registry_url = '/'
-        context['DOCKER_REGISTRY_URL'] = docker_registry_url
-
-    j2 = Jinja2(f"{_LIBD}/templates/.gitlab-ci", context=context)
-    if 'trigger' in target:
-        j2.render('trigger.yml.j2', outfile=f'.gitlab-ci/cache/trigger.yml')
-        print('Gitlab CI trigger config file generated.')
-    if 'package' in target:
-        for name, package in synthesis.package.items():
-            print(f"[{name}] ...", end=' ')
-            j2.render('package.yml.j2', outfile=f'.gitlab-ci/{name}.yml',
-                      context={'package': package})
-            if not package.config.tool and package.tool_user:
-                print(f"{name}.tool ...", end='')
-                j2.render('package.yml.j2', outfile=f'.gitlab-ci/{name}.tool.yml',
-                          context={'package': package, 'FOR_TOOL': True})
-            print(" done.")
-    return None
 
 
 def gitlab_runner_register(args):
@@ -73,11 +42,6 @@ def gitlab_runner_make(args):
 def main():
     parser = argparse.ArgumentParser(prog='Open source software gitlab command tools')
     subs = parser.add_subparsers(dest='sub_command')
-
-    # Generate gitlab-ci config files.
-    cmd = subs.add_parser('generate', help='Generate gitlab runner config')
-    cmd.add_argument('--target', default=None, action='append', help='Generate config for bundle')
-    cmd.set_defaults(func=gitlab_ci_generate)
 
     # Generate gitlab-ci config files.
     cmd = subs.add_parser('runner.register', help='Register gitlab-runner')
