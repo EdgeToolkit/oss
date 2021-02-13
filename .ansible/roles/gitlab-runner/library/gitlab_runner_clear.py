af048842188a324885dd8e8f0652687ff1a75242
Mingyi Zhang <mingyi.z@outlook.com>
2021-02-13 10:06:00 +0800

    .

--- .ansible/roles/gitlab-runner/library/gitlab_runner_clear.py
+++ .ansible/roles/gitlab-runner/library/gitlab_runner_clear.py
@@ -14,7 +14,7 @@ def main():
     proc = subprocess.run(['/bin/bash', '-c', 'echo', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if proc.returncode == 0:
         # 按照ansible 的返回格式定义返回内容,stdout为标准输出,changed代表系统有没有东西被变更,rc=0代表执行成功
-        result = dict(stdout=proc.stdout, changed=changed, rc=0)
+        result = dict(stdout='proc.stdout', changed=changed, rc=0)
         # 使用ansible规则的module实例下的exit_json返回正常内容
         module.exit_json(**result)
     else:
