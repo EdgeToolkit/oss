72b1b1f64907b4a80dcabec6c57fe42d763c663a
Mingyi Zhang <mingyi.z@outlook.com>
2021-04-30 09:30:52 +0800

    [mosquitto] 2

--- mosquitto/test_package/test_package.c
+++ mosquitto/test_package/test_package.c
@@ -13,7 +13,9 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Out of memory.\n");
 		return 1;
 	}
+	printf("create mosquitto instance : %p\n", mosq);
 	mosquitto_lib_cleanup();
+	printf("mosquitto instance cleanup done.\n");
 	return 0;
 }
 
