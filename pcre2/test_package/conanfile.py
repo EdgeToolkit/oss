2943896e6aec5e9b7fdbffdbfd0e410b1d2ea774
Mingyi Zhang <mingyi.z@outlook.com>
2021-04-30 17:09:27 +0800

    .

---
+++ pcre2/test_package/conanfile.py
@@ -0,0 +1,20 @@
+from conans import ConanFile, CMake, tools
+import os
+
+from epm.tools.conan import as_test
+ConanFile = as_test(ConanFile)
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            bin_path = os.path.join("bin", "test_package")
+            arguments = "%sw+ Bincrafters" % ("\\" if self.settings.os == "Windows" else "\\\\")
+            self.run("%s %s" % (bin_path, arguments), run_environment=True)
